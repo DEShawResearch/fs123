639b12bb6bed68d9d1bf3ea60a83c8b1066d507e
johnsalmon <salmonj@deshawresearch.com>
2019-08-25 23:03:17 -0400

    Fix the alpine issues with backward-cpp
    Use a more portable backward.hpp.
    
    Download libdwarf-dev and libelf-dev and configure CPPFLAGS
    and LDLIBS to use them in prereqs.alpine:3.9
    
    Lift the -e up from prereq.* to .travis.yml, aking the prereqs scripts
    more forgiving when used interactively.

--- core123/include/core123/backward.hpp
+++ core123/include/core123/backward.hpp
@@ -1,6 +1,6 @@
-// Downloaded on Aug 15, 2019 from:
-//  https://github.com/bombela/backward-cpp/raw/master/backward.hpp
-//  sha:  9c4c163583e12f79b941a6a64cdba5e1703bfbfd
+// Downloaded on Aug 25, 2019 from https://github.com/bombela/backward-cpp/pull/144
+// commit af2debf91ee954a23a5c44f0b
+
 /*
  * backward.hpp
  * Copyright 2013 Google Inc. All Rights Reserved.
@@ -614,7 +614,7 @@ template <typename TAG>
 class StackTraceImpl {
 public:
 	size_t size() const { return 0; }
-	Trace operator[](size_t) { return Trace(); }
+	Trace operator[](size_t) const { return Trace(); }
 	size_t load_here(size_t=0) { return 0; }
 	size_t load_from(void*, size_t=0) { return 0; }
 	size_t thread_id() const { return 0; }
@@ -1689,7 +1689,7 @@ public:
 
 		Dl_info symbol_info;
 		int dladdr_result = 0;
-#ifndef __ANDROID__
+#if defined(__GLIBC__)
 		link_map *link_map;
 		// We request the link map so we can get information about offsets
 		dladdr_result = dladdr1(trace.addr, &symbol_info,
@@ -1745,7 +1745,7 @@ public:
 			return trace; // sad, we couldn't load the object :(
 		}
 
-#ifndef __ANDROID__
+#if defined(__GLIBC__)
 		// Convert the address to a module relative one by looking at
 		// the module's loading address in the link map
 		Dwarf_Addr address = reinterpret_cast<uintptr_t>(trace.addr) -
