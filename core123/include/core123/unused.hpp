7ba822cfb907871b2cf6ae15733e7f8a2475d0bf
johnsalmon <salmonj@deshawresearch.com>
2019-10-21 16:37:18 -0400

    Make unused() variadic
    Change-Id: I387da8b1ef20b76d7b61f551b7c9c09561e528ff

--- core123/include/core123/unused.hpp
+++ core123/include/core123/unused.hpp
@@ -2,5 +2,5 @@
 
 /// https://herbsutter.com/2009/10/18/mailbag-shutting-up-compiler-warnings/
 namespace core123{
-template <class T> void unused(const T&){}
+template <class ... T> void unused(const T&...){}
 } // namespace core123
