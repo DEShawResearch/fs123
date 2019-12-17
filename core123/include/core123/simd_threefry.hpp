fbb9004c7d8937e58a20656243275d81fa2e7a13
johnsalmon <salmonj@deshawresearch.com>
2019-12-17 13:50:57 -0500

    Add core123/simd_threefry.hpp
    It "seems to work" and it's very, *very* fast.
    It really needs better testing!
    
    Change-Id: I22ed7c96301225232db1a8b0de5c37ef5d442a43

---
+++ core123/include/core123/simd_threefry.hpp
@@ -0,0 +1,242 @@
+// simd_threefry.hpp - allows one to instantiate the templated
+// threefry generators with an element type of one of GCC's Vector
+// Extensions.
+//
+//  https://gcc.gnu.org/onlinedocs/gcc/Vector-Extensions.html
+//
+// This is a work in progress.  It "seems to work".
+//
+// USE WITH CARE.  CHECK CAREFULLY BEFORE TRUSTING RESULTS.
+// See the Notes below for why.
+//
+// The basic idea is to #include this file:
+//
+// #include <core123/simd_threefry.hpp>
+//
+// which typedefs 'uint{64,32}_tx{8,4,2}' to be 8-long, 4-long and
+// 2-long gcc "Vector Extensions" of 64- and 32-bit unsigned integers.
+//
+// After which you can say, e.g.,:
+//
+// threefry<4, uint64_tx8> generator;
+//
+// The resulting generator has counter and key types that are arrays
+// of 4 (the first template argument) elements, each of which is
+// a SIMD vector of 8 (the x8) uint64_t's.  Each "slot" in the
+// simd vector corresponds to a distinct threefry evaluation.  E.g.,
+// in the third slot:
+//   counter[0][3], counter[1][3], counter[2][3] and counter[3][3]
+// will be mixed with:
+//   key[0][3], key[1][3], key[2][3] and key[3][3]
+// to produce the random values that appear in:
+//   output[0][3], output[1][3], output[2][3] and output[3][3]
+//
+// The four output values are exactly the same as would be returned
+// by threefry<4, uint64_t> with the same key and counter values.
+// 
+// All slots (8 of them, in this example) are computed completely
+// independently.
+//
+// Thus "granularity" of threefry<N, uintW_txV> is N*W*V bits.  I.e.,
+// it is constructed with N*W*V key bits and its operator() takes
+// counter argument with N*W*V bits to produce random results with
+// N*W*V output bits.
+//
+
+// Notes:
+//
+// This is a WORK IN PROGRESS.  It "seems to work", but a thorough and
+// convincing unit test is desperately needed.  The vector extensions
+// have been around for a long time, but they're not commonly used by
+// application code.  They're in a gray area from C++ type system's
+// point of view, so mixing them with templates and function
+// overloading is venturing into poorly explored territory.  All in
+// all - CAUTION IS ADVISED.  TEST THOROUGHLY BEFORE USING.
+//
+// GCC compiles simd vectors into correct code even if the target
+// architecture lacks registers and instructions of the required
+// width.  But there's little benefit (and maybe even performance
+// degradation) from using this file unless you also enable generation
+// of code that uses vector instructions.  E.g., something like
+//    -march=haswell
+// to use 256-bit wide AVX2 instructions for uint64_tx4
+// or uint32_tx8.
+// Or:
+//    -march=skylake-avx512
+// to use 512-bit wide AVX512 instructions for uint64_tx8.
+//
+// See:
+//   https://gcc.gnu.org/onlinedocs/gcc/x86-Options.html
+// for which instruction sets are enabled by which -march=options. 
+//
+// When asked to work with vectors that exceed the instruction
+// set's capabilities (e.g., uint64_tx8 without AVX512), gcc
+// issues warnings like:
+//
+//    warning: AVX512F vector return without AVX512F enabled changes the ABI [-Wpsabi]
+//
+// It's possible to silence these warnings with:
+//  #pragma GCC diagnostic push
+//  #pragma GCC diagnostic ignored "-Wpsabi"
+//  #include <core123/simd_threefry.hpp>
+//  #pragma GCC diagnostic pop
+//
+// Since this is header-only code, it tempting to think that ABI
+// issues don't matter - the caller is unavoidably compiled with the
+// same ABI as the callee because they're in the same translation
+// unit.  But I know enough to know I don't know for sure.
+
+#pragma once
+#ifndef __GNUC__
+#error "This header relies on GNUC Vector Extensions.  It is unusable/meaningless without them"
+#endif
+
+#include "threefry.hpp"
+
+using uint64_tx8 = uint64_t __attribute__ ((__vector_size__ (64))); // aka __v8du in <immintrin.h>
+using uint64_tx4 = uint64_t __attribute__ ((__vector_size__ (32))); // aka __v4du in <immintrin.h>
+using uint64_tx2 = uint64_t __attribute__ ((__vector_size__ (16))); // aka __v2du? (not in <immintrin.h>)
+using uint32_tx8 = uint32_t __attribute__ ((__vector_size__ (32))); // aka __v8su in <immintrin.h>
+using uint32_tx4 = uint32_t __attribute__ ((__vector_size__ (16))); // aka __v4su in <immintrin.h>
+using uint32_tx2 = uint32_t __attribute__ ((__vector_size__ (8)));  // aka __v2su? (not in <immintrin.h>)
+
+namespace core123{
+template<>
+inline
+uint64_tx8 rotl<uint64_tx8>(uint64_tx8 x, unsigned s){
+    return (x<<s) | (x>>(64u-s));
+}
+template<>
+inline
+uint64_tx4 rotl<uint64_tx4>(uint64_tx4 x, unsigned s){
+    return (x<<s) | (x>>(64u-s));
+}
+template<>
+inline
+uint64_tx2 rotl<uint64_tx2>(uint64_tx2 x, unsigned s){
+    return (x<<s) | (x>>(64u-s));
+}
+template<>
+inline
+uint32_tx8 rotl<uint32_tx8>(uint32_tx8 x, unsigned s){
+    return (x<<s) | (x>>(32u-s));
+}
+template<>
+inline
+uint32_tx4 rotl<uint32_tx4>(uint32_tx4 x, unsigned s){
+    return (x<<s) | (x>>(32u-s));
+}
+template<>
+inline
+uint32_tx2 rotl<uint32_tx2>(uint32_tx2 x, unsigned s){
+    return (x<<s) | (x>>(32u-s));
+}
+
+// There's really a lot of repetition here... Fix it?
+template<>
+struct threefry_constants<2, uint64_tx2> : public threefry_constants<2, uint64_t>{
+    static constexpr uint64_tx4 KS_PARITY = {threefry_constants<2, uint64_t>::KS_PARITY,
+                                             threefry_constants<2, uint64_t>::KS_PARITY,
+    };
+};
+template<>
+struct threefry_constants<2, uint64_tx4> : public threefry_constants<2, uint64_t>{
+    static constexpr uint64_tx4 KS_PARITY = {threefry_constants<2, uint64_t>::KS_PARITY,
+                                             threefry_constants<2, uint64_t>::KS_PARITY,
+                                             threefry_constants<2, uint64_t>::KS_PARITY,
+                                             threefry_constants<2, uint64_t>::KS_PARITY,
+    };
+};
+template<>
+struct threefry_constants<2, uint64_tx8> : public threefry_constants<2, uint64_t>{
+    static constexpr uint64_tx8 KS_PARITY = {threefry_constants<2, uint64_t>::KS_PARITY,
+                                             threefry_constants<2, uint64_t>::KS_PARITY,
+                                             threefry_constants<2, uint64_t>::KS_PARITY,
+                                             threefry_constants<2, uint64_t>::KS_PARITY,
+                                             threefry_constants<2, uint64_t>::KS_PARITY,
+                                             threefry_constants<2, uint64_t>::KS_PARITY,
+                                             threefry_constants<2, uint64_t>::KS_PARITY,
+                                             threefry_constants<2, uint64_t>::KS_PARITY,
+    };
+};
+template<>
+struct threefry_constants<4, uint64_tx2> : public threefry_constants<4, uint64_t>{
+    static constexpr uint64_tx4 KS_PARITY = {threefry_constants<4, uint64_t>::KS_PARITY,
+                                             threefry_constants<4, uint64_t>::KS_PARITY,
+    };
+};
+template<>
+struct threefry_constants<4, uint64_tx4> : public threefry_constants<4, uint64_t>{
+    static constexpr uint64_tx4 KS_PARITY = {threefry_constants<4, uint64_t>::KS_PARITY,
+                                             threefry_constants<4, uint64_t>::KS_PARITY,
+                                             threefry_constants<4, uint64_t>::KS_PARITY,
+                                             threefry_constants<4, uint64_t>::KS_PARITY,
+    };
+};
+template<>
+struct threefry_constants<4, uint64_tx8> : public threefry_constants<4, uint64_t>{
+    static constexpr uint64_tx8 KS_PARITY = {threefry_constants<4, uint64_t>::KS_PARITY,
+                                             threefry_constants<4, uint64_t>::KS_PARITY,
+                                             threefry_constants<4, uint64_t>::KS_PARITY,
+                                             threefry_constants<4, uint64_t>::KS_PARITY,
+                                             threefry_constants<4, uint64_t>::KS_PARITY,
+                                             threefry_constants<4, uint64_t>::KS_PARITY,
+                                             threefry_constants<4, uint64_t>::KS_PARITY,
+                                             threefry_constants<4, uint64_t>::KS_PARITY,
+    };
+};
+
+template<>
+struct threefry_constants<2, uint32_tx2> : public threefry_constants<2, uint32_t>{
+    static constexpr uint32_tx4 KS_PARITY = {threefry_constants<2, uint32_t>::KS_PARITY,
+                                             threefry_constants<2, uint32_t>::KS_PARITY,
+    };
+};
+template<>
+struct threefry_constants<2, uint32_tx4> : public threefry_constants<2, uint32_t>{
+    static constexpr uint32_tx4 KS_PARITY = {threefry_constants<2, uint32_t>::KS_PARITY,
+                                             threefry_constants<2, uint32_t>::KS_PARITY,
+                                             threefry_constants<2, uint32_t>::KS_PARITY,
+                                             threefry_constants<2, uint32_t>::KS_PARITY,
+    };
+};
+template<>
+struct threefry_constants<2, uint32_tx8> : public threefry_constants<2, uint32_t>{
+    static constexpr uint32_tx8 KS_PARITY = {threefry_constants<2, uint32_t>::KS_PARITY,
+                                             threefry_constants<2, uint32_t>::KS_PARITY,
+                                             threefry_constants<2, uint32_t>::KS_PARITY,
+                                             threefry_constants<2, uint32_t>::KS_PARITY,
+                                             threefry_constants<2, uint32_t>::KS_PARITY,
+                                             threefry_constants<2, uint32_t>::KS_PARITY,
+                                             threefry_constants<2, uint32_t>::KS_PARITY,
+                                             threefry_constants<2, uint32_t>::KS_PARITY,
+    };
+};
+template<>
+struct threefry_constants<4, uint32_tx2> : public threefry_constants<4, uint32_t>{
+    static constexpr uint32_tx4 KS_PARITY = {threefry_constants<4, uint32_t>::KS_PARITY,
+                                             threefry_constants<4, uint32_t>::KS_PARITY,
+    };
+};
+template<>
+struct threefry_constants<4, uint32_tx4> : public threefry_constants<4, uint32_t>{
+    static constexpr uint32_tx4 KS_PARITY = {threefry_constants<4, uint32_t>::KS_PARITY,
+                                             threefry_constants<4, uint32_t>::KS_PARITY,
+                                             threefry_constants<4, uint32_t>::KS_PARITY,
+                                             threefry_constants<4, uint32_t>::KS_PARITY,
+    };
+};
+template<>
+struct threefry_constants<4, uint32_tx8> : public threefry_constants<4, uint32_t>{
+    static constexpr uint32_tx8 KS_PARITY = {threefry_constants<4, uint32_t>::KS_PARITY,
+                                             threefry_constants<4, uint32_t>::KS_PARITY,
+                                             threefry_constants<4, uint32_t>::KS_PARITY,
+                                             threefry_constants<4, uint32_t>::KS_PARITY,
+                                             threefry_constants<4, uint32_t>::KS_PARITY,
+                                             threefry_constants<4, uint32_t>::KS_PARITY,
+                                             threefry_constants<4, uint32_t>::KS_PARITY,
+                                             threefry_constants<4, uint32_t>::KS_PARITY,
+    };
+};
+
+}
---
+++ core123/ut/ut_simd_threefry.cpp
@@ -0,0 +1,87 @@
+#include <core123/simd_threefry.hpp>
+#include <core123/streamutils.hpp>
+#include <core123/timeit.hpp>
+
+#include <numeric>
+#include <iostream>
+#include <chrono>
+
+
+
+using core123::threefry;
+using core123::insbe;
+using core123::timeit;
+using namespace std::chrono;
+
+static constexpr unsigned ROUNDS = 12;
+
+int main(int argc, char **argv){
+    using vcbrng = threefry<4, uint64_tx8, ROUNDS>;
+    vcbrng::domain_type ctr;
+    using eltype = vcbrng::domain_type::value_type;
+    ctr[0] = eltype{00,01,02,03,04,05,06,07};
+    ctr[1] = eltype{10,11,12,13,14,15,16,17};
+    if(ctr.size()>2){
+        ctr[2] = eltype{20,21,22,23,24,25,26,27};
+        ctr[3] = eltype{30,31,32,33,34,35,36,37};
+    }
+    vcbrng tf;
+    auto r = tf(ctr);
+    std::cout << "Vector threefry: sizeof(range_type): " << sizeof(vcbrng::range_type) << "\n";
+    std::cout << std::hex;
+    switch(r.size()){
+    case 4:
+        std::cout << r[0][0] << " " << r[1][0] << " " << r[2][0] << " " << r[3][0] << "\n";
+        std::cout << r[0][1] << " " << r[1][1] << " " << r[2][1] << " " << r[3][1] << "\n";
+        std::cout << r[0][2] << " " << r[1][2] << " " << r[2][2] << " " << r[3][2] << "\n";
+        std::cout << r[0][3] << " " << r[1][3] << " " << r[2][3] << " " << r[3][3] << "\n";
+        break;
+    case 2:
+        throw "Nope.  Not done yet";
+        std::cout << r[0][0] << " " << r[1][0] << "\n";
+        std::cout << r[0][1] << " " << r[1][1] << "\n";
+        std::cout << r[0][2] << " " << r[1][2] << "\n";
+        std::cout << r[0][3] << " " << r[1][3] << "\n";
+        break;
+    }    
+    tf.setkey(r);    // Don't allow the optimizer to exploit zero-valued keys.
+    ctr = r;         // set the key and counter to "random" values
+    static const int LOOP = 2;
+    static const int millis = 2000;
+    eltype sum = {};
+    auto result = timeit(milliseconds(millis),
+           [&ctr, &sum, tf](){
+               eltype incr = {1, 1, 1, 1, 1, 1, 1, 1};
+               for(int i=0; i<LOOP; ++i){
+                   ctr[0] += incr;
+                   auto r = tf(ctr);
+                   sum = std::accumulate(r.begin(), r.end(), sum); // i.e., sum += r[0] + r[1] + .. + r[N-1];
+               }
+           });
+    // Print the sum, so the optimizer can't elide the whole thing!
+    std::cout << "sum = " << sum[0] << " " << sum[1] << " " << sum[2] << " " << sum[3] << " " << sum[4] << " " << sum[5] << " " << sum[6] << " " << sum[7] << "\n";
+    float ns_per_byte = 1.e9*result.sec_per_iter()/LOOP/sizeof(vcbrng::range_type);
+    printf("8-way simd threefry: %lld calls in about %d ms.  %.2f ns per call.  %.3f ns per byte.\n",
+           LOOP*result.count, millis,
+           1.e9*result.sec_per_iter()/LOOP, ns_per_byte);
+    static const float GHz = 3.7;
+    printf("at %.1fGHz that's %.2f cpb or %.1f bytes per cycle\n", GHz, ns_per_byte * GHz, 1./(ns_per_byte * GHz));
+           
+
+    std::cout << "Scalar threefry:\n";
+    {
+    using cbrng = threefry<4, uint64_t, ROUNDS>;
+    cbrng::range_type r;
+    cbrng tf;
+    r = tf({00, 10, 20, 30});
+    std::cout << insbe(r) << "\n";
+    r = tf({01, 11, 21, 31});
+    std::cout << insbe(r) << "\n";
+    r = tf({02, 12, 22, 32});
+    std::cout << insbe(r) << "\n";
+    r = tf({03, 13, 23, 33});
+    std::cout << insbe(r) << "\n";
+    }
+
+    return 0;
+}
