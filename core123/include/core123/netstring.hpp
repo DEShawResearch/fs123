eca159f0ab41f3bde28f29839ed6eb425bed2150
johnsalmon <salmonj@deshawresearch.com>
2020-09-16 21:17:37 -0400

    Overhaul core123/nestring.hpp
    Harden both sput_netstring and svscan_netstring.
    
    Add precise descriptions of what they do.
    
    Much better unit tests.  Previously, there were no unit tests at all
    for svscan_netstring, and there were no tests for error conditions,
    odd corner cases, etc.
    
    Follow the stricter ietf spec, which forbids leading zeros on the
    length.
    
    ut_netstring uses isvstream from svstream.hpp, so svstream now gets
    a workout.
    
    Deep-six fput_netstring.  It was buggy and unused.
    
    Change-Id: I65b2daf26fe66de17f420eca386aaa9efeb38456

--- core123/include/core123/netstring.hpp
+++ core123/include/core123/netstring.hpp
@@ -1,97 +1,148 @@
 #pragma once
 #include <string>
+#include <stdexcept>
+#include <iostream>
+#include <core123/scanint.hpp>
 #include <core123/svto.hpp>
-#include <core123/strutils.hpp>
 
+// Some functions that work with "netstrings" that look like:
+//
+//     <len>:<exactly len bytes of data>,
+//
+// where <len> is written in base-10 decimal.
+//
+// See:
+//   https://cr.yp.to/proto/netstrings.txt
+//   https://tools.ietf.org/html/draft-bernstein-netstrings-02
+//
+// Note that the ietf draft is stricter, explicitly forbidding leading
+// zeros on the length.  '0' may be the first character of the length
+// only if it is the only character in the length.  The code here
+// follows the stricter rule.
 namespace core123 {
 
-// A tiny function to "format" a string as a netstring.
-// The inverse is called svscan_netstring in svto.hpp
+// netstring(str_view) - "format" a str_view into a std::string as a
+// netstring.
 inline std::string netstring(core123::str_view sv){
     auto ret = std::to_string(sv.size());
+    ret.reserve(ret.size() + sv.size() + 2);
     ret.append(1, ':');
     ret.append(sv.data(), sv.size());
     ret.append(1, ',');
     return ret;
 }
 
-inline void fput_netstring(FILE *fp, core123::str_view sv) {
-    if (fprintf(fp, "%zu:", sv.size()) <= 0 || ferror(fp)) {
-        throw std::system_error(errno, std::system_category(),
-                     "failed to write netstring size "+std::to_string(sv.size()));
+// template <bool skip_white=true>
+// size_t svscan_netstring(str_view sv, str_view* svp, size_t start)
+//
+// If the template parameter skip_white is true, then skip over any
+// whitespace at sv[start].  If what follows is a strictly conforming
+// netstring, then set svp to point to netstring's payload and return
+// the offset of the character following the netstring.  Otherwise,
+// throw an exception.  If an exception is thrown, *svp is unchanged.
+template <bool skip_white=true>
+inline size_t svscan_netstring(core123::str_view sv, core123::str_view* svp, size_t start){
+    size_t len;
+    if(skip_white)
+        start = svscan(sv, nullptr, start);
+    // The leading-zero check feels excessive.
+    auto colon = scanint<size_t, 10, false>(sv, &len, start);
+    if(sv.at(start)=='0'){
+        if(len!=0 || colon-start != 1)
+            throw std::runtime_error("svscan_netstring:  leading zeros");
     }
-    if (fwrite(sv.data(), 1, sv.size(), fp) != sv.size() || ferror(fp)) {
-        throw std::system_error(errno, std::system_category(),
-                      "failed to write "+std::to_string(sv.size())+" bytes");
-    }
+        
+    // rely on sv.at() to do bounds-checking.
+    if(sv.at(colon) != ':')
+        throw std::runtime_error("svscan_netstring: no colon");
+    if(sv.at(colon+len+1) != ',')
+        throw std::runtime_error("svscan_netstring: no trailing commma");
+    // No max_size because we're not being asked to allocate space.
+    // We're just being asked to point into a pre-existing str_view.
+    *svp = sv.substr(colon+1, len);
+    return colon+len+2;
 }
 
+// sput_netstring - write the contents of sv to an ostream, formatted
+// as a netstring.  An exception is thrown only in the unlikely event
+// that std::to_string(size_t) throws.  Otherwise, errors are reported
+// in the usual way via the output stream's state.
 inline std::ostream& sput_netstring(std::ostream& out, core123::str_view sv) {
-    // we convert the number ourselves because who knows what state
-    // the stream numeric conversion is in.
-    // 64 bit int needs 20 chars in decimal, so plenty of space
-    char buf[32], *cp=buf+sizeof(buf)-1;
-    *cp-- = '\0';
-    size_t val = sv.size();
-    do {
-        if (cp == buf)
-            throw std::runtime_error("size_t too big for buffer! "+std::to_string(sv.size()));
-        unsigned b = val % 10u;
-        val /= 10u;
-        *cp-- = b + '0';
-    } while (val);
-    out << cp+1 << ':';
+    // use to_string to format the size, sidestepping out's state.
+    // According to C++11, to_string formats as if by sprintf("%u")
+    // (with an appropriate width modifier), and AFAIK, %u formating
+    // is not affected by the LOCALE.  So we should be good...
+    out << std::to_string(sv.size()) << ':';
     out.write(sv.data(), sv.size());
     out << ',';
     return out;
 }
 
-// svscan a netstring into a str_view.  This one feels a bit odd...
-// second argument is a str_view, so the first argument had better
-// hang around as long as svp is needed/used!!
-inline size_t svscan_netstring(core123::str_view sv, core123::str_view* svp, size_t start){
-    size_t len;
-    start = svscan<size_t>(sv, &len, start); // start now points to colon
-    if(sv.size() < 2+len+start)
-        throw std::invalid_argument("svscan_netstring: sv too short: " + std::string(sv.substr(start,30)));
-    if(sv[start] != ':')
-        throw std::invalid_argument("svscan_netstring: did not see colon in: " + std::string(sv.substr(start,30)));
-    if(sv[start+len+1] != ',')
-        throw std::invalid_argument("svscan_netstring: did not see trailing comma in " + std::string(sv.substr(start,30)));
-    *svp = sv.substr(start+1, len);
-    return start+len+2;
-}
-
-inline bool sget_netstring(std::istream& inp, std::string* sp) {
+// sget_netstring(istream& inp, string* sp, size_t max_size=999999999u)
+//
+// Skip leading whitespace in the istream, inp.  If inp is at EOF,
+// return false.  Otherwise, try to parse a netstring from inp.  If
+// the inp contains a netstring with length no more than max_size,
+// read the netstring's payload in *sp.  Upon success, return true,
+// leaving inp's read pointer immediately following the netstring.
+//
+// If any errors are detected (i/o errors, premature eof, input that
+// doesn't strictly conform to the netstring spec, or a netstring
+// longer than max_size), an exception is thrown.  If an exception is
+// thrown, inp's failbit is set, its read pointer is undefined and a
+// default-constructed empty string is swapped into *sp, discarding
+// *sp's previous contents as well as any memory that may have been
+// reserved in *sp for the netstring payload.
+//
+// N.B. the default value of max_size comes from the example code
+// in the specs, which uses scanf("%9u") to read the length.
+inline bool sget_netstring(std::istream& inp, std::string* sp, size_t max_size = 999999999u) try {
     inp >> std::ws;
-    if (inp.eof())
+    if(inp.eof())
         return false;
-    unsigned char c;
+    int c;
     size_t sz = 0;
+    // scanf(%u) and istream >> integral are far too permissive.  Do
+    // the conversion ourselves...
+    bool first_time = true;
     while (1) {
-        c = '\0';
-        inp >> c;
+        c = inp.get();
         if (c == ':')
             break;
-        else if (inp.eof())
-            throw std::runtime_error("sget_netstring got eof when expecting digit, current sz "+std::to_string(sz));
-        else if (!isdigit(c))
-            throw std::runtime_error("sget_netstring got unexpected character "+std::to_string(c)+" when expecting digit, current sz "+std::to_string(sz));
-        sz = sz * 10 + (c - '0');
+        if(!inp.good()) // i.e., any of fail, bad or eof
+            throw std::runtime_error("sget_netstring:  reading from inp failed before end of digits");
+        if(!first_time && sz==0)
+            throw std::runtime_error("sget_netstring:  leading zeros not allowed on length");
+        first_time = false;
+        size_t digit = c - '0';
+        // N.B.  c-'0' is signed, and might be negative, but in that
+        // case, when converted to size_t, it will be (much) larger
+        // than 10.
+        if(digit >= 10)
+            throw std::runtime_error("sget_netstring: expected a digit or colon");
+        size_t oldsz = sz;
+        sz = sz * 10 + digit;
+        if(oldsz > sz)
+            throw std::runtime_error("sget_netstring: length doesn't fit in a size_t");
     }
-    if (sz) {
-        sp->resize(sz);
-        inp.read(&(*sp)[0], sz);
-        if (inp.eof())
-            throw std::runtime_error("sget_netstring got eof when reading string data, "+std::to_string(sz) + " bytes");
-    } else {
-        *sp = "";
-    }
-    inp >> c;
-    if (c != ',') {
-        throw std::runtime_error("sget_netstring got unexpected character "+std::to_string(c)+" when expecting comma, sz "+std::to_string(sz));
-    }
+    if(sz > max_size)
+        throw std::runtime_error("sget_netstring: length exceeds max_size argument");
+    // N.B. sz might be *very* large.  resize might throw bad_alloc, or, worse,
+    // it might succeed, and then inp.read might fail, leaving us with a huge
+    // string containing garbage.  We therefore clear *sp in the catch block.
+    sp->resize(sz);
+    inp.read(&(*sp)[0], sz);
+    if(!inp.good())  // i.e., any of fail, bad or eof
+        throw std::runtime_error("sget_netstring: failed to read " + std::to_string(sz) + " bytes");
+    c = inp.get();
+    if (c != ',')
+        throw std::runtime_error("sget_netstring: did not get terminal comma");
     return true;
-}
+ }catch(std::exception&){
+    // If there's trouble, set inp's failbit and clear *svp.
+    inp.setstate(std::ios::failbit);
+    std::string().swap(*sp);
+    throw;
+ }
 
 } // namespace core123
--- core123/ut/ut_netstring.cpp
+++ core123/ut/ut_netstring.cpp
@@ -1,54 +1,162 @@
 // test program for netstring.hpp
-#include <core123/svstream.hpp>
 #include <core123/netstring.hpp>
 #include <core123/ut.hpp>
+#include <core123/str_view.hpp>
+#include <core123/diag.hpp>
+#include <core123/svstream.hpp>
+#include <core123/svto.hpp>
+#include <core123/complaints.hpp>
 #include <random>
 
-using std::to_string;
-using std::runtime_error;
+using namespace std;
+using core123::str_view;
 using core123::netstring;
+using core123::svscan_netstring;
 using core123::sput_netstring;
 using core123::sget_netstring;
 
 static unsigned long numtests = 0;
 
-void dotest(const char *buf, size_t n) {
+void dotest(str_view svin) {
     numtests += 1;
-    auto s1 = netstring({buf, n});
-    DIAG(_ut, "nstr : " << s1 << "\n");
+    auto s1 = netstring(svin);
+    DIAG(_ut, " nstr : " << s1 << "\n");
     std::stringstream ss;
-    sput_netstring(ss, {buf, n});
+    sput_netstring(ss, svin);
     auto s2 = ss.str();
+    str_view svs2;
+    auto end_of_s2 = svscan_netstring(s2, &svs2, 0);
+    EQUAL(svs2, svin);         // check that svscan_netstring "works"
+    EQUAL(end_of_s2, s2.size()); // and that it points to the end of s2
     DIAG(_ut, "sput : " << s2 << "\n");
-    EQSTR (s1, s2);
+    EQSTR (s1, s2); // netstring and sput_netstring do the same thing
     ss.seekg(0);
-    std::string s3;
-    if (!sget_netstring(ss, &s3))
-        throw runtime_error("ERROR got eof from sget_netstring, s3 is \""+s3+"\"");
-    EQUAL (s3.size(), n);
-    std::string sref(buf, n);
-    EQSTR (s3, sref);
-    sput_netstring(ss, {buf, n});
-    ss << "\n  ";
-    sput_netstring(ss, {buf, n});
-    ss << "  \n";
-    ss.seekg(0);
-    auto k = 0u;
-    while (1) {
-        std::string s4;
-        if (!sget_netstring(ss, &s4))
-            break;
-        EQUAL(s3.size(), n);
-        EQSTR(s3, sref);
-        k += 1;
+    string s3;
+    CHECK(sget_netstring(ss, &s3)); // sget_netstring returns true
+    EQUAL (s3, svin);               // and it matches the input.
+    sput_netstring(ss, svin);  // append another copy of svin to ss
+    ss << "\n  ";              // followed by whitespace
+    sput_netstring(ss, svin);  // followed by another copy
+    ss << "  \n";              // followed by whitespace
+    ss.seekg(0);               // reset the ss read pointer
+    string ssstr = ss.str();   // all of ss, copiled out to a string
+    str_view sssv(ssstr);      // a string_view into ssstr
+    int ncopies = 0;
+    string s4;
+    size_t next = 0;
+    while(sget_netstring(ss, &s4)){
+        EQUAL(s4, svin);       // found another copy of svin in ss.
+        str_view svs4;
+
+        // Let's try again with the scan_netstring with skip_white=false
+        // That should fail.
+        bool caught;
+        try{
+            svscan_netstring<false>(sssv, &svs4, next);
+        }catch(std::exception&){
+            caught = true;
+        }
+        CHECK(caught);
+        // But if we skip over the whitespace, it should succeed:
+        auto nn = core123::svscan(sssv, nullptr, next);
+        nn = svscan_netstring<false>(sssv, &svs4, nn);
+        EQUAL(svs4, svin);
+        
+        // And find it again, but with svscan_netstring
+        next = svscan_netstring(sssv, &svs4, next);
+        EQUAL(svs4, svin);
+        EQUAL(nn, next);
+
+        // And one more time, but with 
+        ncopies += 1;
     }
-    EQUAL(k, 3);
+    EQUAL(ncopies, 3); // we found three copies of svin in ss.
+}
+
+void expect_ok(str_view nsin, size_t expected_offset) try {
+    str_view payloadsv;
+    auto offset = svscan_netstring(nsin, &payloadsv, 0);
+    EQUAL(offset, expected_offset);
+    core123::isvstream svs(nsin);
+    string payloadstr;
+    CHECK(sget_netstring(svs, &payloadstr));
+    EQUAL(expected_offset, size_t(svs.tellg()));
+ }catch(std::exception& e){
+    core123::complain(e, "expect_ok(\"" + std::string(nsin) + "\") threw exception:");
+    CHECK(false);
+ }
+
+void expect_error(str_view nsin) try {
+    bool caught = false;
+    try{
+        str_view payloadsv;
+        svscan_netstring(nsin, &payloadsv, 0);
+    }catch(std::exception&){
+        caught = true;
+    }
+    CHECK(caught);
+
+    core123::isvstream svs(nsin);
+    std::string payloadstr(512, 'x'); // big enough to not fit in SSO
+    try{
+        sget_netstring(svs, &payloadstr);
+    }catch(std::exception&){
+        caught = true;
+    }
+    // check that we both threw an exception and set the failbit
+    // and left the payload with the capacity of a default string.
+    CHECK(caught);
+    CHECK(!svs);
+    CHECK(payloadstr.empty());
+    EQUAL(payloadstr.capacity(), std::string().capacity());
+ }catch(std::exception& e){
+    core123::complain(e, "expect_error(\"" + std::string(nsin) + "\") threw an *unexpected* exception:");
+    CHECK(false);
+ }
+    
+void test_corner_cases(){
+    // Test a few cases that should work:
+    expect_ok("3:,,,,,,,,,", 6);
+    expect_ok("   3:,,,,,,,,,", 9);
+    expect_ok("3:\0\0\0,   "s, 6);  // note the 's'.  It's a C++ string literal with NULs.
+    expect_ok("\t3:abc,XTRA", 7); // and various kinds of whitespace.
+    expect_ok("\r3:abc,", 7); // and various kinds of whitespace.
+    expect_ok("\t\r\n 3:abc,JUNK", 10); // and various kinds of whitespace.
+    expect_ok("\f 3:abc,\0\0\0"s, 8); // and various kinds of whitespace.
+    
+    // Test that svscan_netstring and sget_netstring properly handle
+    // a selection of contrived error conditions and corner cases.
+    expect_error("3:1b,");    // too short
+    expect_error("3:1b,  ");  // too short
+    expect_error("  3:1b,  ");  // too short
+    expect_error("3:abcd,"); // wrong length
+    expect_error("3:abc");   // premature EOF
+    expect_error("99:abcdefghijk,"); // premature EOF
+    expect_error("999999999:abcdefg,"); // will allocate ~1GB, but then discard before throwing.
+    expect_error("4000000000:abcdefg"); // should not even try to allocate memory.  Check with valgrind.
+    expect_error("3,abc,"); // missing colon
+    expect_error("-1:abc,");  // minus sign. Nope.
+    expect_error("-0:,");     // minus sign. Nope.
+    expect_error("+0:,");     // plus sign. Nope.
+    expect_error("abc3:abc,"); // not a number
+    expect_error("3abc:abc,"); // not a number
+    expect_error("\03:abc,"); // NUL isn't a number either.
+    expect_error("3\0:abc,"); // even when it comes after a number.
+    expect_error("1\00:1234567890,"); // or in the middle
+    expect_error("0x3:abc,"); // hex isn't a number either.
+    expect_error("03:abc,");  // leading zeros aren't allowed either
+    expect_error("0000000003:abc,");  // even a lot of leading zeros
+    expect_error("00:,");       // even "just" leading zeros
+    expect_error("010:1234567890,");  // but leading zeros DO NOT make it octal!
 }
 
 int
 main(int, char **)
 {
     const size_t bufsz = 200;
+    // It's not clear that we learn much from calling do_test a
+    // million times.  But until something better comes along, it only
+    // takes a few seconds...
     unsigned long ntests =  1000000;
     auto cp = getenv("NTESTS");
     if (cp) {
@@ -60,17 +168,19 @@ main(int, char **)
         srandom(std::stol(cp));
     }
     
-    dotest("", 0);
-    dotest("", 1);
-    dotest("hello", 5);
+    test_corner_cases();
+
+    dotest("");
+    dotest("hello");
+    dotest(std::string(12345, 'x'));
     for(auto i = 0ul; i < ntests; i++) {
         size_t n = random() % 200;
         for (size_t j = 0; j < n; j++) {
             buf[j] = static_cast<char>((random() % 95) + 32);
         }
-        auto sbuf = std::string(buf, n);
+        auto sbuf = string(buf, n);
         DIAG(_ut, i << " buf \"" << sbuf << "\"");
-        dotest(buf, n);
+        dotest(str_view(buf, n));
     }
     return utstatus();
 }
