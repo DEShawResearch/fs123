5a30da230c520e55982566c770cbfb45a60a5a30
johnsalmon <salmonj@deshawresearch.com>
2021-02-22 01:57:48 -0500

    Allow compilation with gcc-7
    Which allows building with bionic's build-essential.
    Also fiddle with interativity of focal prereqs.

--- include/fs123/requirements.hpp
+++ include/fs123/requirements.hpp
@@ -8,17 +8,17 @@
 
 #if defined(__clang__)
 #  if __clang_major__ < 7
-#    error "fs123 requires at least clang-7 or gcc-8 and -std=c++17"
+#    error "fs123 requires at least clang-7 or gcc-7 and -std=c++17"
 #    include "Compilation stopped because fs123 requires at least clang-7 or gcc-8 and -std=c++17"
 #  endif
 #elif defined(__ICC) // N.B.  icpc also defines __GNUC__, so this must come first
 #  if __ICC < 1900
-#    error "fs123 requires at least icc-19 or clang-7 or gcc-8 and -std=c++17"
+#    error "fs123 requires at least icc-19 or clang-7 or gcc-7 and -std=c++17"
 #    include "Compilation stopped because fs123 requires at least icc-19 clang-7 or gcc-8 and -std=c++17"
 #  endif
 #elif defined(__GNUC__)
-#  if __GNUC__ < 8
-#    error "fs123 requires at least clang-7 or gcc-8 and -std=c++17"
-#    include "Compilation stopped because fs123 requires at least clang-7 or gcc-8 and -std=c++17"
+#  if __GNUC__ < 7
+#    error "fs123 requires at least clang-7 or gcc-7 and -std=c++17"
+#    include "Compilation stopped because fs123 requires at least clang-7 or gcc-7 and -std=c++17"
 #  endif
 #endif
